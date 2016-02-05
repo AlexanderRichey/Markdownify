6b38428f9c239ebe452c658087e7473b3c3d0e2e
Alexander Richey <alexander.richey@gmail.com>
2016-02-05 16:05:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+#Markdownify
+
+This is a simple ruby script that converts HTML WordpressDotCom posts that have been imported with [Jekyll-Import](http://import.jekyllrb.com/docs/wordpressdotcom/) into Markdown. It renders only the following HTML elements in Markdown:
+
+* paragraphs
+* blockquotes
+* links
+* emphasis (aka *italics*)
+* front-matter (only *layout* and *title*; no tags, date, etc.)
+
+This means Markdownify does *NOT* render images, inline code, bolded text, lists, tables, etc. The reason for Markdownify's limited capabilities is that I wrote it with my blog posts in mind; and they're pretty simple.
+
+##Using Markdownify
+
+Markdownify takes in `.html` files and outputs new `.md` files of the same names. Load up the script in Terminal and pass in your `.html` files as arguments, all at once or one at a time.
+```
+#All at once:
+$ ruby markdownify.rb test_posts/*.html
+#One at a time:
+$ ruby markdownify.rb test_posts/post_1.html #=> post_1.md
+```
+New `.md` files will be created alongside your old `.html` files. Be sure to check them for errors, since HTML made with Wordpress's web app is usually pretty messy.
+
+##Images, Other HTML Elements & Stuff
+
+##### Images
+
+If there are images in any of your `.html` files, the line on which they occur will be rendered as `***THERE WAS AN IMAGE HERE***`. I did this because my blog posts usually don't have images. It would be pretty easy to make Markdownify support images, so, if you want to, fork it... or something.
+
+##### Other HTML Elements
+
+Other HTML elements will be either rendered as they are or not rendered at all - it depends on where they occur in the line.
+
+##### Other Stuff
+
+Also, if you want to keep your tags or something, just modify the #front_matter method and consider forking it.
+
+##Apology
+
+I've only been coding for a couple months, so be nice and thanks for reading!
---
+++ markdownify.rb
@@ -0,0 +1,90 @@
+#For after running jekyll-import.
+
+class String
+  def markdownify
+    output = ""
+
+    if image?(self)
+      output << "***THERE WAS AN IMAGE HERE***"
+    elsif paragraph?(self)
+      output << italics(links(paragraphs(self)))
+    elsif blockquote?(self)
+      output << italics(links(paragraphs(blockquotes(self))))
+    else
+      output << front_matter(self)
+    end
+
+    output
+  end
+
+  def paragraph?(string)
+    front_matter(string) == "" && string.start_with?("<p>") ? true : false
+  end
+
+  def blockquote?(string)
+    front_matter(string) == "" && string.start_with?("<blockquote>") ? true : false
+  end
+
+  def image?(string)
+    n = 0
+    while n < string.length - 3
+      return true if string[n..(n + 3)] == "<img"
+      n += 1
+    end
+    false
+  end
+
+  def front_matter(string)
+    string.split.first == "---" || string.split.first == "layout:" || string.split.first == "title:" ? string : ""
+  end
+
+  def paragraphs(string)
+    string.gsub("<p>", "").gsub("</p>", "\n")
+  end
+
+  def blockquotes(string)
+    string.gsub("<blockquote>", ">").gsub("</blockquote>", "")
+  end
+
+  def italics(string)
+    string.gsub(" <em>", " *").gsub("<em>", "*").gsub(" </em>", "* ").gsub("</em>", "*")
+  end
+
+  def md_link(string)
+    url_start = string.index('<a href=') + 8
+    url_end = string.index('>', url_start) - 1
+    url = string[url_start..url_end]
+
+    linked_text_start = url_end + 2
+    linked_text_end = string.index('</a>', linked_text_start) - 1
+    linked_text = string[linked_text_start..linked_text_end]
+
+    @link_start = url_start - 8
+    @link_end = linked_text_end + 4
+
+    "[#{linked_text}](#{url})"
+  end
+
+  def links(string)
+    return string unless string.include?('<a href=')
+
+    new_link = md_link(string)
+    old_link = string[@link_start..@link_end]
+
+    new_string = string.sub(old_link, new_link)
+    links(new_string)
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  input_files = ARGV
+
+  input_files.each do |input|
+    post_name = input.sub(".html", ".md")
+    new_file = File.open(post_name, "w")
+
+    File.foreach(input) do |line|
+      new_file.puts line.markdownify unless line.markdownify == ""
+    end
+  end
+end
